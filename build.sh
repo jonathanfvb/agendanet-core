4fe5bfd05099e9b82d6675c9fd41408ee8ca6e61
Jonathan Braz <jonathan.fvb@hotmail.com>
2022-10-29 14:38:25 -0300

    Removendo build bref

--- build.sh
+++
@@ -1,5 +0,0 @@
-docker rm -f teste-bref
-docker build -f Dockerfile.local -t jonathanbraz/bref .
-docker run -d -p 8090:8090 --env-file ./.env --name teste-bref jonathanbraz/bref
-
-#docker exec -it teste-bref sh
