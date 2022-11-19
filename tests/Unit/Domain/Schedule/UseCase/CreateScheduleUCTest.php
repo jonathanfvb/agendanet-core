b7d24977acff9b6060290f86256e7db06016caab
Jonathan Braz <jonathan.fvb@hotmail.com>
2022-11-19 10:23:30 -0300

    Corrigindo teste unitário CreateScheduleUCTest

--- tests/Unit/Domain/Schedule/UseCase/CreateScheduleUCTest.php
+++ tests/Unit/Domain/Schedule/UseCase/CreateScheduleUCTest.php
@@ -4,7 +4,10 @@ namespace Tests\Unit\Domain\Schedule\UseCase;
 
 use Agendanet\App\Commons\Http\Exceptions\UnprocessableEntityException;
 use Agendanet\Domain\Doctor\Repository\DoctorRepositoryInterface;
+use Agendanet\Domain\Doctor\Repository\DoctorScheduleRepositoryInterface;
 use Agendanet\Domain\Schedule\DTO\CreateScheduleRequest;
+use Agendanet\Domain\Schedule\Factory\ScheduleFactoryInterface;
+use Agendanet\Domain\Schedule\Repository\ScheduleRepositoryInterface;
 use Agendanet\Domain\Schedule\UseCase\CreateScheduleUC;
 use PHPUnit\Framework\TestCase;
 
@@ -12,10 +15,25 @@ final class CreateScheduleUCTest extends TestCase
 {
     private DoctorRepositoryInterface $doctorRepositoryMock;
     
+    private DoctorScheduleRepositoryInterface $doctorScheduleRepositoryMock;
+    
+    private ScheduleRepositoryInterface $scheduleRepositoryMock;
+    
+    private ScheduleFactoryInterface $scheduleFactoryMock;
+    
     public function setUp(): void
     {
         $this->doctorRepositoryMock = $this->createMock(
             DoctorRepositoryInterface::class
+        );
+        $this->doctorScheduleRepositoryMock = $this->createMock(
+            DoctorScheduleRepositoryInterface::class
+        );
+        $this->scheduleRepositoryMock = $this->createMock(
+            ScheduleRepositoryInterface::class
+        );
+        $this->scheduleFactoryMock = $this->createMock(
+            ScheduleFactoryInterface::class
         );
         parent::setUp();
     }
@@ -39,7 +57,12 @@ final class CreateScheduleUCTest extends TestCase
     
     private function getCreateScheduleUC(): CreateScheduleUC
     {
-        return new CreateScheduleUC($this->doctorRepositoryMock);
+        return new CreateScheduleUC(
+            $this->doctorRepositoryMock,
+            $this->doctorScheduleRepositoryMock,
+            $this->scheduleRepositoryMock,
+            $this->scheduleFactoryMock
+        );
     }
     
     private function getCreateScheduleRequest(): CreateScheduleRequest
@@ -48,7 +71,7 @@ final class CreateScheduleUCTest extends TestCase
             '41988887777',
             'John Doe',
             'aaa-123',
-            '2022-12-31 23:59:00'
+            '2022-12-31 23:59'
         );
     }
 }
